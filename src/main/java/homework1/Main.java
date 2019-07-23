e3983b11fb5c80995f97ebbf9cdbd02df7416617
Марина Пятерня <marina.pyaternya@gmail.com>
2019-07-23 23:58:45 +0300

    Initial commit

---
+++ src/main/java/homework1/Main.java
@@ -0,0 +1,58 @@
+package homework1;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Java";
+        System.out.println(name);
+
+        byte byteNumber = 25;
+        System.out.println(byteNumber);
+
+        int number = 666;
+        System.out.println(number);
+
+        short sNumber = 29000;
+        System.out.println(sNumber);
+
+        Long lNumber = 15000L;
+        System.out.println(lNumber);
+
+        double dNumber = 168.5;
+        System.out.println(dNumber);
+
+        Float fNumber = 255.6655F;
+        System.out.println(fNumber);
+
+        char chars = '%';
+        System.out.println(chars);
+
+        boolean bool1 = true;
+        System.out.println(bool1);
+
+        boolean bool2 = false;
+        System.out.println(bool2);
+        System.out.println();
+
+        String Phone = "Phone: ";
+        String numberPhone = "1234567890";
+        System.out.println(Phone.concat(numberPhone));
+        System.out.println();
+
+        String name2 = "S circle = ";
+        double squareCircle;
+        double pi=3.14;
+        int radius=9;
+        squareCircle=pi*(radius*radius);
+        System.out.print(name2);
+        System.out.println(squareCircle);
+        System.out.println();
+
+        String name3 = "S triangle = ";
+        int a=86;
+        double h=1.5;
+        System.out.print(name3);
+        System.out.println((a/2)*h);
+
+
+    }
+}
